39d6a0c4aa1fc26caef48b4cacd62964b121f8f2
重田早紀 <8221sa@gmail.com>
2023-10-30 23:18:51 +0900

    fmt.Println

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// fmt.Println
+	// テキストを一行で出力
+	fmt.Println("Hello, Golang!") // -> "Hello, Golang!"
+	// 引数を複数取れる。スペースで区切られて連結
+	fmt.Println("My", "Name", "Is", "Taro") // -> "My Name Is Taro"
+}
