ee5f91abad56b12399afbd7f97b1ac5ee84635ad
重田早紀 <8221sa@gmail.com>
2023-10-30 23:19:04 +0900

    fmt.Printf

--- main.go
+++ main.go
@@ -10,4 +10,9 @@ func main() {
 	fmt.Println("Hello, Golang!") // -> "Hello, Golang!"
 	// 引数を複数取れる。スペースで区切られて連結
 	fmt.Println("My", "Name", "Is", "Taro") // -> "My Name Is Taro"
+
+	// fmt.Printf
+	// 引数を埋め込む
+	fmt.Printf("10進数=%d 2進数=%b 8進数=%o 16進数=%x\n", 17, 17, 17, 17)
+	// 10進数=17 2進数=10001 8進数=21 16進数=11
 }
